5760108e7430c7be535ef4819d2b2d78af7f1c8d
Deliwe08 <gugulethu.deliwe@gmail.com>
2023-03-17 10:59:51 -0700

    Size of the grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-        char a;
-	int b;
-	float f;
-	long int c;
-	long long int d;
+char a;
+int b;
+long int c;
+long long d;
+float f;
 
 
 	printf("Size of char: %ld byte(s)\n", sizeof(char));
 	printf("size of int: %ld byte(s)\n", sizeof(int));
-	printf("size of float: %ld byte(s)\n", sizeof(float));
-	printf("size of double: %ld byte(s)\n", sizeof(double));
-	printf("size of long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of long int: %ld byte(s)\n", sizeof(float));
+	printf("size of long long int: %ld byte(s)\n", sizeof(double));
+	printf("size of float %ld byte(s)\n", sizeof(long long int));
 	return (0);
 }
