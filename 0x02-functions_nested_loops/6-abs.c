b0e8a5a162de8782da5300ceabbcf7dce53ca0e5
Deliwe08 <gugulethu.deliwe@gmail.com>
2023-03-22 01:26:35 -0700

    3 colors

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returns the value of an absolute value of an integer
+ * @n: the int to check 
+ * Return: absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	r = _abs (-1)
+	{
+	return (n);
+	}
+	return (-n);
+}
