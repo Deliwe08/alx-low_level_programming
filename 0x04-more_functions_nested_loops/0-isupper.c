be6fa385a3978ab5568dfd085e087bfe2e2d9d56
Deliwe08 <gugulethu.deliwe@gmail.com>
2023-03-23 02:06:52 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _isupper - check if letter c is uppercase
+ * @c: the number to be checked
+ *
+ * Returns: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
